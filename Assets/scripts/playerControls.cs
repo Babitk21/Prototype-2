e07d63be6ac57cf067982c3564936927e1556f57
Babitk21 <77804801+Babitk21@users.noreply.github.com>
2021-04-15 12:24:47 +0530

    prototype-2 di

---
+++ Assets/scripts/playerControls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour
+{
+    private float horizontalInput=100.0f;
+    private float speed = 10.0f;
+    float xRange = 20;
+    public GameObject ProjectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ProjectilePrefab, transform.position, transform.rotation);
+        }
+    }
+}
